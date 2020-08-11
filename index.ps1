e2f16952941615dc61959f01bba1ba190ad5c063
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-08-11 18:36:42 -0500

    rename abspath to path

--- index.ps1
+++ index.ps1
@@ -1,4 +1,4 @@
 Import-Module -Name ./src/object.psm1
 Import-Module -Name ./src/which.psm1
 Import-Module -Name ./src/symlink.psm1
-Import-Module -Name ./src/abspath.psm1
+Import-Module -Name ./src/path.psm1
