26f7a61bd77f437f2cbddc39d5cc54278af1e993
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-08-11 19:01:39 -0500

    fix the relative path

--- index.ps1
+++ index.ps1
@@ -1,4 +1,4 @@
-Import-Module -Name ./src/object.psm1
-Import-Module -Name ./src/which.psm1
-Import-Module -Name ./src/symlink.psm1
-Import-Module -Name ./src/path.psm1
+Import-Module -Name $PSScriptRoot/src/object.psm1
+Import-Module -Name $PSScriptRoot/src/which.psm1
+Import-Module -Name $PSScriptRoot/src/symlink.psm1
+Import-Module -Name $PSScriptRoot/src/path.psm1
