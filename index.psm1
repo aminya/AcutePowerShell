8e3d87f34c22adf79e05d20b73f2b90078934389
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-05-25 02:33:48 -0500

    add ls_size

--- index.psm1
+++ index.psm1
@@ -4,3 +4,4 @@ Import-Module -Name $PSScriptRoot/src/symlink.psm1 -DisableNameChecking
 Import-Module -Name $PSScriptRoot/src/path.psm1 -DisableNameChecking
 Import-Module -Name $PSScriptRoot/src/time.psm1 -DisableNameChecking
 Import-Module -Name $PSScriptRoot/src/git.psm1 -DisableNameChecking
+Import-Module -Name $PSScriptRoot/src/fs.psm1 -DisableNameChecking
