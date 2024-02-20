cd1abcabebc0d1d3d01ca3e79cc8b7207ad1145d
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-02-20 00:55:40 -0800

    fix: fix the starship copy command

--- src/extensions.psm1
+++ src/extensions.psm1
@@ -28,5 +28,9 @@ if (![ExperimentalFeature]::IsEnabled("PSLoadAssemblyFromNativeCode")) { Enable-
 
 new-alias -Name sudo -Value gsudo
 
-cp "./src/starship.toml" "~/.config/starship.toml"
+
+# if config does not exist, copy it
+if (!(Test-Path -Path "~/.config/starship.toml")) {
+    Copy-Item -Path "$PSScriptRoot/src/starship.toml" -Destination "~/.config/starship.toml"
+}
 Invoke-Expression (& "C:\Program Files\starship\bin\starship" init powershell)
