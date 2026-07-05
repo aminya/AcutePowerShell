c41e2dd2681be0093fa56d92c2fce58675fbec00
Amin Ya <aminyahyaabadi74@gmail.com>
2026-07-04 23:03:21 -0700

    fix: update msvc

--- src/msvc.psm1
+++ src/msvc.psm1
@@ -1,6 +1,6 @@
 function enable_msvc() {
     $ORIG_PWD = $PWD
-    & { Import-Module "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell -Arch amd64 -HostArch amd64 -VsInstallPath "C:/Program Files/Microsoft Visual Studio/2022/Community/" }
+    & { Import-Module "C:/Program Files/Microsoft Visual Studio/18/Community/Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell -Arch amd64 -HostArch amd64 -VsInstallPath "C:/Program Files/Microsoft Visual Studio/18/Community/" }
     cd $ORIG_PWD
 }
 Export-ModuleMember -Function enable_msvc
