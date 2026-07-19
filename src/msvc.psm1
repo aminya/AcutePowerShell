df2c9873775b96da8a5e9fe47c69f0a8b3b8fc85
Amin Ya <aminyahyaabadi74@gmail.com>
2026-07-19 05:06:57 -0700

    fix: support spaces in pwd

--- src/msvc.psm1
+++ src/msvc.psm1
@@ -1,7 +1,10 @@
 function enable_msvc() {
-    $ORIG_PWD = $PWD
-    & { Import-Module "C:/Program Files/Microsoft Visual Studio/18/Community/Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell -Arch amd64 -HostArch amd64 -VsInstallPath "C:/Program Files/Microsoft Visual Studio/18/Community/" }
-    cd $ORIG_PWD
+    $ORIG_PWD = "$PWD"
+    & { 
+        Import-Module 'C:/Program Files/Microsoft Visual Studio/18/Community/Common7\Tools\Microsoft.VisualStudio.DevShell.dll'; 
+        Enter-VsDevShell -Arch "amd64" -HostArch "amd64" -VsInstallPath 'C:/Program Files/Microsoft Visual Studio/18/Community/';
+    }
+    cd "$ORIG_PWD"
 }
 Export-ModuleMember -Function enable_msvc
 
